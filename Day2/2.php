ae9732e4464bbb45e90fca53184705351cedff00
kristian.robe <kristian.robe@bullhorn.com>
2019-12-07 21:33:57 -0500

    Day 2 Part 1 attempt 2

--- Day2/2.php
+++ Day2/2.php
@@ -5,11 +5,11 @@ $input = explode(",", file_get_contents('inputs/input.txt'));
 $input[1] = 12;
 $input[2] = 2;
 for ($i = 0; $i < sizeof($input); $i = $i + 4) {
-    if($i == 1) {
+    if($input[$i] == 1) {
         $input[$i + 3] = $input[$i + 1] + $input[$i + 2];
-    } elseif($i == 2) {
+    } elseif($input[$i] == 2) {
         $input[$i + 3] = $input[$i + 2] * $input[$i + 2];
-    } elseif($i == 99) {
+    } elseif($input[$i] == 99) {
         break;
     } else {
         echo "Something went wrong";
