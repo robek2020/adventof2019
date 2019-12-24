c6aad3ba281f7ad6b912e2f191db24df805036dc
kristian.robe <kristian.robe@bullhorn.com>
2019-12-24 15:51:02 -0500

    Day 2 all done

--- Day2/2.php
+++ Day2/2.php
@@ -5,9 +5,9 @@ function runIntcodeFromInput(&$input, $noun, $verb) {
     $input[2] = $verb;
     for ($i = 0; $i < sizeof($input); $i = $i + 4) {
         if($input[$i] == 1) {
-            $input[$i + 3] = $input[$i + 1] + $input[$i + 2];
+            $input[$input[$i + 3]] = $input[$input[$i + 1]] + $input[$input[$i + 2]];
         } elseif($input[$i] == 2) {
-            $input[$i + 3] = $input[$i + 2] * $input[$i + 2];
+            $input[$input[$i + 3]] = $input[$input[$i + 1]] * $input[$input[$i + 2]];
         } elseif($input[$i] == 99) {
             break;
         } else {
@@ -19,10 +19,18 @@ function runIntcodeFromInput(&$input, $noun, $verb) {
 
 $input = explode(",", file_get_contents('inputs/input.txt'));
 $inputPart1 = $input;
-runIntcodeFromInput($input, 12, 2);
+$inputPart2 = $input;
+runIntcodeFromInput($inputPart1, 12, 2);
 
-print_r("Answer 1: " . $input[0] . "\r\n");
+print_r("<div>Answer 1: " . $inputPart1[0] . "</div>");
 
-$input2 = $input;
-
-
+for ($i = 0; $i <= 99; $i++) {
+    for ($j = 0; $j <= 99; $j++) {
+        $inputPart2 = $input;
+        runIntcodeFromInput($inputPart2, $i, $j);
+        if ($inputPart2[0] === 19690720) {
+            print_r("<div>Answer 2:  " . ((100*$i)+$j) . "</div>");
+            break;
+        }
+    }
+}
