242983475029202deb52b0008155bb5bbe6a6ff4
kristian.robe <kristian.robe@bullhorn.com>
2019-12-24 15:05:35 -0500

    Day 2 Part 1 refactor to use common function

--- Day2/2.php
+++ Day2/2.php
@@ -1,20 +1,28 @@
 <?php
 
-$input = explode(",", file_get_contents('inputs/input.txt'));
-
-$input[1] = 12;
-$input[2] = 2;
-for ($i = 0; $i < sizeof($input); $i = $i + 4) {
-    echo "<div>Currently processing position " . $i . " with input " . $input[$i] . "</div>";
-    if($input[$i] == 1) {
-        $input[$i + 3] = $input[$i + 1] + $input[$i + 2];
-    } elseif($input[$i] == 2) {
-        $input[$i + 3] = $input[$i + 2] * $input[$i + 2];
-    } elseif($input[$i] == 99) {
-        break;
-    } else {
-        echo "Something went wrong";
-        break;
+function runIntcodeFromInput($input, $noun, $verb) {
+    $input[1] = $noun;
+    $input[2] = $verb;
+    for ($i = 0; $i < sizeof($input); $i = $i + 4) {
+        if($input[$i] == 1) {
+            $input[$i + 3] = $input[$i + 1] + $input[$i + 2];
+        } elseif($input[$i] == 2) {
+            $input[$i + 3] = $input[$i + 2] * $input[$i + 2];
+        } elseif($input[$i] == 99) {
+            break;
+        } else {
+            echo "Something went wrong";
+            break;
+        }
     }
+    return $input;
 }
-echo $input[0];
+
+$input = explode(",", file_get_contents('inputs/input.txt'));
+$inputPart1 = $input;
+
+print_r("Answer 1: " . runIntcodeFromInput($inputPart1, 12, 2)[0] . "\r\n");
+
+$input2 = $input;
+
+
